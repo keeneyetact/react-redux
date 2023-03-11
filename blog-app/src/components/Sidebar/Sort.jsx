9e441e21ee5c3530e9233faa94ea877661ab9f64
tiger-eagle <elaineelainefletcher@gmail.com>
2023-03-11 09:18:17 +0600

    blog app sort-redux

--- blog-app/src/components/Sidebar/Sort.jsx
+++ blog-app/src/components/Sidebar/Sort.jsx
@@ -1,24 +1,19 @@
-import React, { useEffect, useState } from 'react'
-import { useDispatch } from 'react-redux'
+import React from 'react'
+import { useDispatch, useSelector } from 'react-redux'
 import { sortSelected } from '../../features/sort/sortSlice'
 
 
 const Sort = () => {
   const dispatch = useDispatch()
-  const [sortValue, setSortValue] = useState('')
+  const { sort } = useSelector(state => state.sort)
 
-  const handleSelect = (e) => {
-    setSortValue(e.target.value)
-  }
-
-  useEffect(() => {
-    dispatch(sortSelected(sortValue))
-  }, [dispatch, sortValue])
 
   return (
     <div className="sidebar-content">
     <h4>Sort</h4>
-    <select onChange={handleSelect} name="sort" id="lws-sort" className="w-full max-w-[150px] border-2 rounded-md text-gray-500">
+    <select onChange={(e) => dispatch(sortSelected(e.target.value))} 
+            value={sort} name="sort" id="lws-sort" 
+            className="w-full max-w-[150px] border-2 rounded-md text-gray-500">
       <option value="">Default</option>
       <option value="createdAt">Newest</option>
       <option value="likes">Most Liked</option>
