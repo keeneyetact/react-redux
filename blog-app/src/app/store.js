3694cf0f971e688019de60122f09d697b50dd7d6
tiger-eagle <elaineelainefletcher@gmail.com>
2023-03-11 08:10:39 +0600

    blog app refactor

--- blog-app/src/app/store.js
+++ blog-app/src/app/store.js
@@ -1,5 +1,4 @@
 import { configureStore } from '@reduxjs/toolkit';
-import counterReducer from '../features/counter/counterSlice';
 import blogsReducer from '../features/blogs/blogsSlice'
 import relatedBlogsReducer from '../features/relatedBlogs/relatedBlogsSlice'
 import blogReducer from '../features/blog/blogSlice'
@@ -8,7 +7,6 @@ import sortReducer from '../features/sort/sortSlice';
 
 export const store = configureStore({
   reducer: {
-    counter: counterReducer,
     blogs: blogsReducer,
     blog: blogReducer,
     relatedBlogs: relatedBlogsReducer,
