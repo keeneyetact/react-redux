0713ad688aaa98c1a6d4bc933c00194145ae2ecb
tiger-eagle <elaineelainefletcher@gmail.com>
2023-03-23 15:32:21 +0600

    task-manager delete-task

--- task-manager-with-rtk-query/src/features/task/taskApi.js
+++ task-manager-with-rtk-query/src/features/task/taskApi.js
@@ -4,7 +4,6 @@ export const taskApi = apiSlice.injectEndpoints({
     endpoints: (builder) => ({
         getTasks: builder.query({
             query: () => "/tasks",
-            keepUnusedDataFor: 300,
         }),
         addTask: builder.mutation({
             query: ({data}) => ({
@@ -12,21 +11,20 @@ export const taskApi = apiSlice.injectEndpoints({
                 method: 'POST',
                 body: data
             }),
-            async onQueryStarted(args, {queryFulfilled, dispatch}) {
+            async onQueryStarted(data, {queryFulfilled, dispatch}) {
                 console.log('here')
                 try {
                     const task = await queryFulfilled;
                     if(task?.data) {
-                        console.log(task)
+                        console.log('task', task)
                         // update task cache pessimistcally start
                         dispatch(
                             apiSlice.util.updateQueryData(
                                 "getTasks",
+                                data,
                                 (draft) => {
                                     console.log({draft, task})
-                                    return {
-                                        data: [...draft.data, ...task.data]
-                                    }
+                                    Object.assign(draft, data)
                                 }
                             )
                         );
@@ -37,11 +35,39 @@ export const taskApi = apiSlice.injectEndpoints({
                     console.log(error)
                 }
             }
+        }),
+        deleteTask: builder.mutation({
+            query: (id) => ({
+                url: `/tasks/${id}`,
+                method: 'DELETE',
+            }),
+            async onQueryStarted(id, { queryFulfilled, dispatch}) {
+                // optimistic cache update
+                const deleteResult = dispatch(
+                    apiSlice.util.updateQueryData(
+                        "getTasks",
+                        id,
+                        (draft) => {
+                            console.log("draft before update:", draft);
+                            draft.data = draft.data.filter(d => d.id !== id.toString())
+                        }
+                    )
+                )
+                console.log('here')
+                // optimistic cache update end
+                try {
+                    console.log('here')
+                    await queryFulfilled(deleteResult)
+                } catch (error) {
+                    deleteResult.undo()
+                }
+            }
         })
     })
 })
 
 export const {
     useGetTasksQuery,
-    useAddTaskMutation
+    useAddTaskMutation,
+    useDeleteTaskMutation
 } = taskApi
