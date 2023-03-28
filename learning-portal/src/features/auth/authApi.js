c59ca53afbf098cfd08ad1a84301cb0fbfef5f4b
tiger-eagle <elaineelainefletcher@gmail.com>
2023-03-28 10:37:28 +0600

    learning-portal auth-feature

---
+++ learning-portal/src/features/auth/authApi.js
@@ -0,0 +1,75 @@
+import { apiSlice } from "../api/apiSlice";
+import { userLoggedIn } from './authSlice'
+
+export const authApi = apiSlice.injectEndpoints({
+    endpoints: (builder) => ({
+        register: builder.mutation({
+            query: (data) => ({
+                url: "/register",
+                method: "POST",
+                body: data
+            }),
+            async onQueryStarted(arg, { queryFulfilled, dispatch }) {
+                try {
+                    const result = await queryFulfilled;
+
+                    // setting current user in the localstorage
+                    localStorage.setItem(
+                        "auth",
+                        JSON.stringify({
+                            accessToken: result.data.accessToken,
+                            user: result.data.user,
+                            role: result.data.user?.role,
+                        })
+                    );
+
+                    // keeping current user info in the redux store
+                    dispatch(userLoggedIn({
+                        accessToken: result.data.accessToken,
+                        user: result.data.user,
+                        role: result.data.user?.role,
+                    }));
+
+                } catch (error) {
+                    // error case
+                }
+            }
+        }),
+        login: builder.mutation({
+            query: (data) => ({
+                url: "/login",
+                method: "POST",
+                body: data
+            }),
+            async onQueryStarted(arg, { queryFulfilled, dispatch}) {
+                try {
+                    const result = await queryFulfilled;
+
+                    // setting localstorage
+                    localStorage.setItem(
+                        "auth",
+                        JSON.stringify({
+                            accessToken: result.data.accessToken,
+                            user: result.data.user,
+                            role: result.data.user?.role,
+                        })
+                    );
+
+                    // keeping current user info in the redux store
+                    dispatch(userLoggedIn({
+                        accessToken: result.data.accessToken,
+                        user: result.data.user,
+                        role: result.data.user?.role,
+                    }));
+                } catch (error) {
+                    // error case
+                }
+            }
+        })
+    })
+})
+
+export const {
+    useLoginMutation,
+    useRegisterMutation
+} = authApi;
