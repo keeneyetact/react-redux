7a0f328b0b2d16561d10617433815a0f73cf8616
tiger-eagle <elaineelainefletcher@gmail.com>
2023-03-28 12:15:23 +0600

    learning-portal error-loading-ui

--- learning-portal/src/components/comon/Error.jsx
+++ learning-portal/src/components/comon/Error.jsx
@@ -1,7 +1,7 @@
 export default function Error({ message }) {
     return (
         <div className="flex items-center">
-            <div className="relative rounded-md bg-red-200 max-w-xl px-4 py-2 text-red-800 rounded shadow w-full">
+            <div className="relative flex justify-center rounded-md bg-red-200 max-w-xl px-4 py-2 text-red-800 rounded shadow w-full">
                 <span className="block text-sm">{message}</span>
             </div>
         </div>
