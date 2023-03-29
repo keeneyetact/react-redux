0bb166984ea9470d1dae7db42aed3663a90df334
tiger-eagle <elaineelainefletcher@gmail.com>
2023-03-29 15:21:54 +0600

    learning-portal get-add-video done

--- learning-portal/src/features/api/apiSlice.js
+++ learning-portal/src/features/api/apiSlice.js
@@ -1,10 +1,19 @@
 import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react'
 
+const baseQuery = fetchBaseQuery({
+    baseUrl: process.env.REACT_APP_API_URL,
+    prepareHeaders: async (headers, { getState, endpoint }) => {
+        const token = getState()?.auth?.accessToken;
+        if(token) {
+            headers.set("Authorization", `Bearer ${token}`)
+        }
+
+        return headers
+    }
+})
 
 export const apiSlice = createApi({
     reducerPath: "api",
-    baseQuery: fetchBaseQuery({
-        baseUrl: process.env.REACT_APP_API_URL
-    }),
+    baseQuery,
     endpoints: (builder) => ({})
 })
--- learning-portal/src/pages/admin/AddVideo.jsx
+++ learning-portal/src/pages/admin/AddVideo.jsx
@@ -1,11 +1,46 @@
 import React from 'react'
+import { useEffect } from 'react'
+import { useState } from 'react'
+import { useNavigate } from 'react-router-dom'
 import LearningPortal from '../../assets/image/learningportal.svg'
+import Error from '../../components/common/Error'
+import { useAddVideoMutation } from '../../features/videos/videosApi'
 
 const AddVideo = () => {
+    const navigate = useNavigate()
+    const [ title, setTitle ] = useState('')
+    const [ description, setDescription ] = useState('')
+    const [ url, setUrl ] = useState('')
+    const [ views, setViews ] = useState('')
+    const [ duration, setDuration] = useState('')
+    const [ error, setError ] = useState('')
+
+    const [ addVideo, {isLoading, isSuccess, error: addError }] = useAddVideoMutation()
 
     const handleSubmit = (e) => {
         e.preventDefault()
+        setError('')
+        addVideo({
+            data: {
+                title,
+                description,
+                url,
+                views,
+                duration,
+                createdAt: new Date()
+            }
+        })
     }
+
+    useEffect(()=> {
+        if(addError?.data) setError("Something went wrong. Please, give a refresh and try again!!!")
+        // console.log(addError)
+    },[addError])
+
+    useEffect(()=> {
+          if(isSuccess) navigate('/admin/videos')
+    },[isSuccess, navigate])
+      
   return (
     <div className="mt-8 py-4 bg-primary h-screen place-items-center">
         <div className="mx-auto max-w-md md:max-w-lg px-5 lg:px-0">
@@ -20,30 +55,32 @@ const AddVideo = () => {
                     <div>
                         <label htmlFor="title" className="sr-only">Title here..</label>
                         <input id="title" name="title" type="text" required
+                               value={title} onChange={(e)=> setTitle(e.target.value)}
                                className="video-form-input rounded-t-md" placeholder="Enter Video Title Here.." />
                     </div>
                     <div>
                         <label htmlFor="description" className="sr-only">Description here..</label>
-                        <input id="description" name="description" type="text" required
-                               className="video-form-input rounded-t-md" placeholder="Enter Video Description Here.." />
+                        <textarea id="description" name="description" type="text" required row="3"
+                                  value={description} onChange={(e)=> setDescription(e.target.value)}
+                                  className="video-form-input rounded-t-md" placeholder="Enter Video Description Here.." />
                     </div>
                     <div>
                         <label htmlFor="url" className="sr-only">URL here..</label>
                         <input id="url" name="url" type="text" required
-                            //    value={email} onChange={(e)=> setEmail(e.target.value)}
+                               value={url} onChange={(e)=> setUrl(e.target.value)}
                                className="video-form-input rounded-t-md" placeholder="Enter Video URL Here.." />
                     </div>
                     <div className="grid grid-cols-2 gap-4">
                     <div className="relative">
                         <label htmlFor="views" className="sr-only">Views here..</label>
                         <input id="views" name="views" type="text" required
-                            //    value={email} onChange={(e)=> setEmail(e.target.value)}
+                               value={views} onChange={(e)=> setViews(e.target.value)}
                                className="video-form-input rounded-t-md" placeholder="Enter Video Views Here.." />
                     </div>
                     <div className="relative">
                         <label htmlFor="duration" className="sr-only">Duration here..</label>
                         <input id="duration" name="duration" type="text" required
-                            //    value={email} onChange={(e)=> setEmail(e.target.value)}
+                               value={duration} onChange={(e)=> setDuration(e.target.value)}
                                className="video-form-input rounded-t-md" placeholder="Enter Video Duration Here.." />
                     </div>
                     </div>
@@ -51,13 +88,13 @@ const AddVideo = () => {
                 </div>
 
                 <div>
-                    <button type="submit"
+                    <button type="submit" disabled={isLoading}
                         className="group relative w-full flex justify-center py-2 px-4 border border-transparent text-sm font-medium rounded-md text-white bg-violet-600 hover:bg-violet-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-violet-500">
                         Add Video
                     </button>
                 </div>
 
-                {/* {error !== "" && <Error message={error} />} */}
+                {error !== "" && <Error message={error} />}
             </form>
         </div>
     </div>
