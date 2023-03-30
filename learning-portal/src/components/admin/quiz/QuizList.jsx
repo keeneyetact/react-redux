0c89ada9bfd9def6de08664afd640c0e4d9f62a7
tiger-eagle <elaineelainefletcher@gmail.com>
2023-03-30 14:45:25 +0600

    learning-portal quiz-component with quiz

---
+++ learning-portal/src/components/admin/quiz/QuizList.jsx
@@ -0,0 +1,33 @@
+import React from 'react'
+import { useGetQuizzesQuery } from '../../../features/quizzes/quizzesApi'
+import SingleQuiz from './SingleQuiz'
+
+const QuizList = () => {
+    const {data} = useGetQuizzesQuery()
+  return (
+    <div className="px-3 py-20 bg-opacity-10">
+                <div className="w-full flex">
+                    <button className="btn ml-auto">Add Quiz</button>
+                </div>
+                <div className="overflow-x-auto mt-4">
+                    <table className="divide-y-1 text-base divide-gray-600 w-full">
+                        <thead>
+                            <tr>
+                                <th className="table-th">Question</th>
+                                <th className="table-th">Video</th>
+                                <th className="table-th justify-center">Action</th>
+                            </tr>
+                        </thead>
+
+                        <tbody className="divide-y divide-slate-600/50">
+                            
+                            {data && data.map(d => <SingleQuiz key={d.id} quiz={d} /> )}
+                            
+                        </tbody>
+                    </table>
+                </div>
+            </div>
+  )
+}
+
+export default QuizList
