70501802c2be3a595b9b7e086e475615ff94bd2a
tiger-eagle <elaineelainefletcher@gmail.com>
2023-04-06 17:58:23 +0600

    learning-portal quiz loading error

--- learning-portal/src/components/admin/quiz/QuizList.jsx
+++ learning-portal/src/components/admin/quiz/QuizList.jsx
@@ -2,21 +2,37 @@ import React from 'react'
 import { useNavigate } from 'react-router-dom'
 import { useGetQuizzesQuery } from '../../../features/quizzes/quizzesApi'
 import SingleQuiz from './SingleQuiz'
+import Loading from '../../common/Loading'
+import Error from '../../common/Error'
+import NoContent from '../../common/NoContent'
 
 const QuizList = () => {
-    const {data} = useGetQuizzesQuery()
+    const {data: quizData, isLoading, isError, error} = useGetQuizzesQuery()
     const navigate = useNavigate()
 
     const handleClick = (e) => {
         e.preventDefault();
         navigate('/admin/quizzes/add')
     }
-  return (
-    <div className="px-3 py-20 bg-opacity-10">
-                <div className="w-full flex">
-                    <button onClick={handleClick} className="btn ml-auto">Add Quiz</button>
-                </div>
-                <div className="overflow-x-auto mt-4">
+
+    // decide what to render
+    let content;
+
+    if(isLoading) {
+        content = <Loading />;
+    }
+    
+    if(!isLoading && !isError) {
+        content = <Error message={error?.data || 'Something Went Wrong...'} />;
+    }
+
+    if(!isLoading && !isError && quizData?.length === 0 ) {
+        content = <NoContent message={'No, Quiz Found...! To Add Quiz Click Add Quiz Button.'} />
+    }
+
+    if(!isLoading && !isError && quizData?.length > 0 ) {
+        content = (
+            <div className="overflow-x-auto mt-4">
                     <table className="divide-y-1 text-base divide-gray-600 w-full">
                         <thead>
                             <tr>
@@ -28,11 +44,21 @@ const QuizList = () => {
 
                         <tbody className="divide-y divide-slate-600/50">
                             
-                            {data && data.map(d => <SingleQuiz key={d.id} quiz={d} /> )}
+                            {quizData && quizData.map(d => <SingleQuiz key={d.id} quiz={d} /> )}
                             
                         </tbody>
                     </table>
+            </div>
+        )
+    }
+
+
+  return (
+    <div className="px-3 py-20 bg-opacity-10">
+                <div className="w-full flex">
+                    <button onClick={handleClick} className="btn ml-auto">Add Quiz</button>
                 </div>
+                {content}
             </div>
   )
 }
