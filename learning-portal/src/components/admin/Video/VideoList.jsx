f8decdb11d346174f186aacc86462e75c308dc2c
tiger-eagle <elaineelainefletcher@gmail.com>
2023-04-06 23:18:03 +0600

    learning-portal video add edit loading error

--- learning-portal/src/components/admin/Video/VideoList.jsx
+++ learning-portal/src/components/admin/Video/VideoList.jsx
@@ -1,26 +1,50 @@
 import React from 'react'
 import { useGetVideosQuery } from '../../../features/videos/videosApi'
 import SingleVideo from './SingleVideo'
+import Error from '../../../components/common/Error'
+import Loading from '../../../components/common/Loading'
+import NoContent from '../../../components/common/NoContent'
 
 const VideoList = () => {
     const {data, isError, isLoading} = useGetVideosQuery()
-    console.log(data)
+
+    // decide what to render
+    let content = null;
+
+    if(isLoading) {
+      content= <Loading />
+    }
+
+    if(!isLoading && isError) {
+      content = <Error message={"Something Went Wrong... Please, Refresh The Page!!!"} />
+    }
+
+    if(!isLoading && !isError && data?.length === 0) {
+      content = <NoContent message={"No Video Found... To Add Video Click Add Video Button...!!!"} />
+    }
+
+    if(!isLoading && !isError && data?.length > 0) {
+      content = (
+        <table className="divide-y-1 text-base divide-gray-600 w-full">
+        <thead>
+            <tr>
+                <th className="table-th">Video Title</th>
+                <th className="table-th">Description</th>
+                <th className="table-th">Action</th>
+            </tr>
+        </thead>
+
+        <tbody className="divide-y divide-slate-600/50">
+            {data && data.map(video => <SingleVideo key={video.id} video={video} />)}
+        </tbody>
+    </table>
+      )
+    }
+    
   return (
     <div className="overflow-x-auto mt-4">
-                    <table className="divide-y-1 text-base divide-gray-600 w-full">
-                        <thead>
-                            <tr>
-                                <th className="table-th">Video Title</th>
-                                <th className="table-th">Description</th>
-                                <th className="table-th">Action</th>
-                            </tr>
-                        </thead>
-
-                        <tbody className="divide-y divide-slate-600/50">
-                            {data && data.map(video => <SingleVideo key={video.id} video={video} />)}
-                        </tbody>
-                    </table>
-                </div>
+        {content}               
+    </div>
   )
 }
 
--- learning-portal/src/pages/admin/video/EditVideo.jsx
+++ learning-portal/src/pages/admin/video/EditVideo.jsx
@@ -1,111 +1,48 @@
 import React from 'react'
-import { useEffect } from 'react'
-import { useState } from 'react'
-import { useNavigate, useParams } from 'react-router-dom'
+import {  useParams } from 'react-router-dom'
+import VideoForm from '../../../components/admin/Video/VideoForm'
 import LearningPortal from '../../../assets/image/learningportal.svg'
 import Error from '../../../components/common/Error'
-import { useEditVideoMutation, useGetVideoQuery } from '../../../features/videos/videosApi'
+import Loading from '../../../components/common/Loading'
+import NoContent from '../../../components/common/NoContent'
+import { useGetVideoQuery } from '../../../features/videos/videosApi'
 
 const EditVideo = () => {
 
-    const navigate = useNavigate()
     const { id } = useParams()
-    const [ title, setTitle ] = useState('')
-    const [ description, setDescription ] = useState('')
-    const [ url, setUrl ] = useState('')
-    const [ views, setViews ] = useState('')
-    const [ duration, setDuration] = useState('')
-    const [ error, setError ] = useState('')
+    const { data: videoData, isLoading, isError } = useGetVideoQuery(id)
 
-    const { data } = useGetVideoQuery(id)
-    const [ editVideo, { isLoading, isSuccess, error: editError } ] = useEditVideoMutation()
-  
-    const handleSubmit = (e) => {
-        e.preventDefault()
-        editVideo({
-            id,
-            data: {
-                title,
-                description,
-                url,
-                views,
-                duration
-            }
-        })
+    // decide what to render
+    let content;
+
+    if(isLoading) {
+        content = <Loading />;
     }
 
-    useEffect(()=> {
-        setTitle(data?.title)
-        setDescription(data?.description)
-        setUrl(data?.url)
-        setViews(data?.views)
-        setDuration(data?.duration)
-    },[data])
+    if(!isLoading && isError) {
+        content = <Error message={'Something Went Wrong... Please, Try Again Later...!'} />;
+    }
 
-    useEffect(()=> {
-        if(editError?.data) setError("Something went wrong. Please, give a refresh and try again!!!")
-        // console.log(addError)
-    },[editError])
+    if(!isLoading && isError && !videoData?.id ) {
+        content = <NoContent message={'The Video your trying to edit is not currently available...!'} />
+    }
 
-    useEffect(()=> {
-          if(isSuccess) navigate('/admin/videos')
-    },[isSuccess, navigate])
+    if(!isLoading && !isError && videoData?.id ) {
+        content = (
+            <VideoForm videoData={videoData} id={id} />
+        )
+    }
 
   return (
     <div className="mt-8 py-4 bg-primary h-screen place-items-center">
     <div className="mx-auto max-w-md md:max-w-lg px-5 lg:px-0">
         <div>
             <img className="h-12 mx-auto" src={LearningPortal} alt="" />            
-            <h1 className="mt-6 text-center text-3xl font-extrabold text-slate-100">
+            <h1 className="mt-6 mb-6 text-center text-3xl font-extrabold text-slate-100">
                 Edit Video
             </h1>
         </div>
-        <form className="mt-8 space-y-6" onSubmit={handleSubmit} >
-            <div className="rounded-md shadow-sm -space-y-px">
-                <div>
-                    <label htmlFor="title" className="sr-only">Title here..</label>
-                    <input id="title" name="title" type="text" required
-                           value={title} onChange={(e)=> setTitle(e.target.value)}
-                           className="video-form-input rounded-t-md" placeholder="Enter Video Title Here.." />
-                </div>
-                <div>
-                    <label htmlFor="description" className="sr-only">Description here..</label>
-                    <textarea id="description" name="description" type="text" required row="3"
-                              value={description} onChange={(e)=> setDescription(e.target.value)}
-                              className="video-form-input rounded-t-md" placeholder="Enter Video Description Here.." />
-                </div>
-                <div>
-                    <label htmlFor="url" className="sr-only">URL here..</label>
-                    <input id="url" name="url" type="text" required
-                           value={url} onChange={(e)=> setUrl(e.target.value)}
-                           className="video-form-input rounded-t-md" placeholder="Enter Video URL Here.." />
-                </div>
-                <div className="grid grid-cols-2 gap-4">
-                <div className="relative">
-                    <label htmlFor="views" className="sr-only">Views here..</label>
-                    <input id="views" name="views" type="text" required
-                           value={views} onChange={(e)=> setViews(e.target.value)}
-                           className="video-form-input rounded-t-md" placeholder="Enter Video Views Here.." />
-                </div>
-                <div className="relative">
-                    <label htmlFor="duration" className="sr-only">Duration here..</label>
-                    <input id="duration" name="duration" type="text" required
-                           value={duration} onChange={(e)=> setDuration(e.target.value)}
-                           className="video-form-input rounded-t-md" placeholder="Enter Video Duration Here.." />
-                </div>
-                </div>
-
-            </div>
-
-            <div>
-                <button type="submit" disabled={!data || isLoading}
-                    className="group relative w-full flex justify-center py-2 px-4 border border-transparent text-sm font-medium rounded-md text-white bg-violet-600 hover:bg-violet-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-violet-500">
-                    Update Video
-                </button>
-            </div>
-
-            {error !== "" && <Error message={error} />}
-        </form>
+        {content}
     </div>
 </div>
   )
